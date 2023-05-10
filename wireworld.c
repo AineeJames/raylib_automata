1bcfbb779a25ce4eafaf18acc4f1d1b48da61436
AineeJames <olsenaiden33@gmail.com>
2023-05-09 17:51:57 -0700

    added cell size

--- wireworld.c
+++ wireworld.c
@@ -1,20 +1,19 @@
 #include "raylib.h"
+#include <stdio.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
+#define CELL_SIZE 10
+#define GRID_WIDTH (WINDOW_WIDTH / CELL_SIZE)
+#define GRID_HEIGHT (WINDOW_HEIGHT / CELL_SIZE)
 
 int main() {
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib [core] example - basic window");
   SetTargetFPS(60);   
   while(!WindowShouldClose()) {
     BeginDrawing();
-      ClearBackground(RAYWHITE);
+      ClearBackground(BLACK);
       DrawFPS(0,0);
-      int broslength = MeasureText("BRUH MOMENT", 20);
-      char brodebug[100];
-      sprintf(brodebug, "Bro is %d long", broslength);
-      DrawText(&brodebug, 100,100, 20, BLACK);
-      DrawText("BRUH MOMENT", (WINDOW_WIDTH / 2) - broslength,(WINDOW_HEIGHT / 2) - broslength, 20, DARKPURPLE);
     EndDrawing();
   }
   CloseWindow();
