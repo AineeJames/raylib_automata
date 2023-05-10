4a3477f46467bb392046f1819841982fbbead9e8
AineeJames <olsenaiden33@gmail.com>
2023-05-09 17:13:31 -0700

    added window

--- wireworld.c
+++ wireworld.c
@@ -4,6 +4,8 @@
 #define WINDOW_HEIGHT 600
 
 int main() {
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib [core] example - basic window");
+  SetTargetFPS(60);   
   while(!WindowShouldClose()) {
     BeginDrawing();
       ClearBackground(RAYWHITE);
