8b04f3d9f29e9bfd0d354c1431eab0e4b630c82d
James Wilcock <jameswilcock8@gmail.com>
2023-05-09 17:44:24 -0700

    bro test

--- wireworld.c
+++ wireworld.c
@@ -9,6 +9,12 @@ int main() {
   while(!WindowShouldClose()) {
     BeginDrawing();
       ClearBackground(RAYWHITE);
+      DrawFPS(0,0);
+      int broslength = MeasureText("BRUH MOMENT", 20);
+      char brodebug[100];
+      sprintf(brodebug, "Bro is %d long", broslength);
+      DrawText(&brodebug, 100,100, 20, BLACK);
+      DrawText("BRUH MOMENT", (WINDOW_WIDTH / 2) - broslength,(WINDOW_HEIGHT / 2) - broslength, 20, DARKPURPLE);
     EndDrawing();
   }
   CloseWindow();
