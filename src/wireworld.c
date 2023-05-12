ed1b973a9a7aca369179c11abba983c8ce82fe34
James Wilcock <jameswilcock8@gmail.com>
2023-05-12 12:54:09 -0700

    beginning to split up sim.h

--- src/wireworld.c
+++ src/wireworld.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "sim.h"
 #include "styles/style_dark.h"
 #include <stdbool.h>
 #include <stdio.h>
@@ -18,22 +19,8 @@
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-#define CELL_SIZE 20
-#define GRID_WIDTH (WINDOW_WIDTH / CELL_SIZE)
-#define GRID_HEIGHT (WINDOW_HEIGHT / CELL_SIZE)
 
 // const Color COPPER_PASTEL = {221, 140, 88, 255};
-Color state_colors[] = {(Color){221, 140, 88, 255}, BLUE, RED, BLACK};
-
-const char *state_names[] = {"WIRE", "HEAD", "TAIL", "ERASE"};
-
-typedef enum { WIRE, HEAD, TAIL, EMPTY } cell_state;
-
-typedef struct {
-  int x;
-  int y;
-} cell_coord;
-
 cell_state cell_grid[GRID_WIDTH][GRID_HEIGHT];
 cell_state next_cell_grid[GRID_WIDTH][GRID_HEIGHT];
 bool playing = false;
@@ -91,6 +78,7 @@ int main() {
     else if (IsKeyPressed(KEY_L) && !showSaveWindow)
       showLoadWindow = true;
     Vector2 mousePos = GetMousePosition();
+
     for (int i = 0; i < 4; i++) {
       if (CheckCollisionPointRec(mousePos, colorsRects[i])) {
         stateMouseHover = i;
